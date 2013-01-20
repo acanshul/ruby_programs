3f6000d167be485d131bc9d6c2efbe350eaff3c4
venkat <venkat.ch.bi@gmail.com>
2013-01-20 13:18:29 +0530

    palindrome program

---
+++ 003.palindrome.rb
@@ -0,0 +1,31 @@
+# Palindrom of a given number (and string)
+
+=begin
+# Number Palindrome
+print "Enter a number: "
+  n = gets.to_i
+  temp = n
+  rem = 0
+  rev = 0
+  while(n > 0)
+    rem = n % 10
+	rev = (rev * 10) + rem
+	n = n / 10
+  end
+  if ( rev == temp )
+    puts "Your number is Palindrome!"
+  else
+    puts "Your number is not a Palindrome!"
+  end
+=end
+  
+# String Palindrome
+print "Enter your string: "
+  str = gets
+  temp = str
+  rev = str.reverse
+  if rev == temp
+    puts "Given string is a Palindrome!"
+  else
+	puts "Given string is not a Palindrome!"
+  end
