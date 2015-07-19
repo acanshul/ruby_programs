0b5373b7d5a938b1e2df982548bdfac0d9b5a00c
git-venkat <git.venkat@gmail.com>
2015-07-19 22:37:42 +0530

    format changes

--- 0012.prime_number.rb
+++ 0012.prime_number.rb
@@ -1,8 +1,8 @@
-# Find whethe the given number is prime number or not
+# Find whether the given number is prime number or not
 
 =begin
 What is Prime number?
-	If a number is having only 1 and itself as divisors, called prime number.
+  If a number is having only 1 and itself as divisors, called prime number.
 Ex: 2, 3, 7.. 
 =end
 
@@ -13,11 +13,12 @@ i = 2
 count = 0
 while(i < n/2)
   if(n % i) == 0
-  count += 1
-  break
+    count += 1
+    break
   end
   i += 1
 end
+
 if(count == 0 && n != 1)
   print "#{n} is a Prime number!"
 else
